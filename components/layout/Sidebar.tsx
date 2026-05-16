57f1a17bf330131945b80e1e0a9ac88457535500
kennguyentx <kennguyen08@gmail.com>
2026-05-16 15:57:15 -0500

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -9,7 +9,6 @@ const nav = [
   { href: '/pipeline',   label: 'Pipeline',   icon: Kanban },
   { href: '/deals',      label: 'Deals',      icon: Building2 },
   { href: '/contacts',   label: 'Contacts',   icon: Users },
-  { href: '/digest',     label: 'Digest',     icon: BarChart3 },
   { href: '/intake',     label: 'CIM Intake', icon: Zap },
 ]
 
