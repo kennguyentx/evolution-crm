164f83876767b16889a9bf19146a0d9cb9c3d563
kennguyentx <kennguyen08@gmail.com>
2026-05-17 17:41:31 -0500

    Add files via upload

---
+++ app/manifest.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Evolution Strategy CRM',
+    short_name: 'ES CRM',
+    description: 'Deal and portfolio management',
+    start_url: '/pipeline',
+    display: 'standalone',
+    background_color: '#f2f3f5',
+    theme_color: '#4F284B',
+    orientation: 'portrait',
+    icons: [
+      { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+      { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+    ],
+  }
+}
